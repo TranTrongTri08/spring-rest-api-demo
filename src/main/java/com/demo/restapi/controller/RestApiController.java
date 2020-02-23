fdbebdaa3aac34eb5b73f8b7be54b9e92df15beb
Tri Tran <tri_trong.t@kbtg.tech>
2020-02-23 15:45:30 +0700

    Add spring profiles

--- src/main/java/com/demo/restapi/controller/RestApiController.java
+++ src/main/java/com/demo/restapi/controller/RestApiController.java
@@ -1,13 +1,17 @@
 package com.demo.restapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/helloWorld")
 public class RestApiController {
+	
+	@Value("${app.env}")
+    private String env;
 
 	@GetMapping
 	public String helloWorld() {
-		return "Hello world";
+		return "Hello world. " + env;
 	}
 }
